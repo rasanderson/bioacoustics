427ba6e38bc05039a968807f79d5085262f24160
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2022-04-08 15:23:14 +0100

    Try and do it in Python but falling over on import of torch

---
+++ simple_torch_classify.py
@@ -0,0 +1,21 @@
+# https://pytorch.org/tutorials/intermediate/speech_command_classification_with_torchaudio_tutorial.html
+
+# Uncomment the line corresponding to your "runtime type" to run in Google Colab
+
+# CPU:
+!pip install pydub torch==1.7.0+cpu torchvision==0.8.1+cpu torchaudio==0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
+
+# GPU:
+# !pip install pydub torch==1.7.0+cu101 torchvision==0.8.1+cu101 torchaudio==0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchaudio
+import sys
+
+import matplotlib.pyplot as plt
+import IPython.display as ipd
+
+from tqdm import tqdm
