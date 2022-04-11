3ae64c69f509b303df88ec540754c0eaa619462b
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2022-04-11 16:02:43 +0100

    Added wrens. Still a problem with MFCCFunction from behavioR as NaNs produced. Unclear how function is working and no documentation. May need to switch to tuneR:melfcc with it uses under the hood.

--- simple_mfcc.R
+++ simple_mfcc.R
@@ -15,22 +15,28 @@ SONG_DIR <- "datasets/xeno_canto/"
 
 blackbird <- query_xc(qword = 'Turdus merula type:song len:5-25', download = FALSE)
 chiffchaff <- query_xc(qword = 'Phylloscopus collybita cnt:"united kingdom" len:5-25', download = FALSE)
+wren      <- query_xc(qword = 'Troglodytes troglodytes type:song len:5-25', download = FALSE)
 
 # Create subfolders in your RStudio Project for song calls and alarm calls
 dir.create(file.path(paste0(SONG_DIR, "blackbird")), recursive = TRUE)
 dir.create(file.path(paste0(SONG_DIR, "chiffchaff")), recursive = TRUE)
+dir.create(file.path(paste0(SONG_DIR, "wren")), recursive = TRUE)
 
 # Download the .MP3 files into two separate sub-folders
 query_xc(X = blackbird, path=paste0(SONG_DIR, "blackbird"))
 query_xc(X = chiffchaff, path=paste0(SONG_DIR, "chiffchaff"))
+query_xc(X = wren, path=paste0(SONG_DIR, "wren"))
 
 # If you want to convert to .wav
 mp32wav(path=paste0(SONG_DIR, "blackbird"), dest.path=paste0(SONG_DIR, "blackbird"))
 mp32wav(path=paste0(SONG_DIR, "chiffchaff"), dest.path=paste0(SONG_DIR, "chiffchaff"))
+mp32wav(path=paste0(SONG_DIR, "wren"), dest.path=paste0(SONG_DIR, "wren"))
 unwanted_mp3 <- dir(path=paste0(SONG_DIR, "blackbird"), pattern="*.mp3")
 file.remove(paste0(SONG_DIR, "blackbird/", unwanted_mp3))
 unwanted_mp3 <- dir(path=paste0(SONG_DIR, "chiffchaff"), pattern="*.mp3")
 file.remove(paste0(SONG_DIR, "chiffchaff/", unwanted_mp3))
+unwanted_mp3 <- dir(path=paste0(SONG_DIR, "wren"), pattern="*.mp3")
+file.remove(paste0(SONG_DIR, "wren/", unwanted_mp3))
 
 
 blackbird_wav <- readWave(paste0(SONG_DIR, "blackbird/Turdus-merula-243908.wav"))
@@ -48,6 +54,9 @@ file.copy(from=paste0(paste0(SONG_DIR, "blackbird/"),
           to=paste0(SONG_DIR, "for_analysis"))
 file.copy(from=paste0(paste0(SONG_DIR, "chiffchaff/"),
                       list.files(paste0(SONG_DIR, "chiffchaff"))),
+          to=paste0(SONG_DIR, "for_analysis"))
+file.copy(from=paste0(paste0(SONG_DIR, "wren/"),
+                      list.files(paste0(SONG_DIR, "wren"))),
           to=paste0(SONG_DIR, "for_analysis"))
 
 
