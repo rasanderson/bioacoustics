fbca20cb5cc57e5f5f6bc16ec1722a72cb087edb
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2022-04-13 17:32:32 +0100

    randomly able to cut out 5s bits of audio

--- audio_fn.R
+++ audio_fn.R
@@ -61,3 +61,69 @@ my_spec <- function (sound.wav, min.freq = 500, max.freq = 8000, Colors = "Color
                axes = T, useRaster = TRUE, main = main))
   }
 }
+
+# for debugging rnd_mix function
+input.dir1 <- paste0(SONG_DIR, "blackbird")
+input.dir2 <- paste0(SONG_DIR, "chiffchaff")
+input.dir3 <- paste0(SONG_DIR, "wren")
+seed=123
+nrand.in=5
+nrand.out=3
+output.dir1 <- "one"
+output.dir2 <- "two"
+output.dir3 <- "three"
+n.sec <- 5
+samp.rate <- 44100
+
+# Need to adapt resamp to avoid error being thrown if new samp freq unchanged
+my_resamp <- function (wave, f, g, channel = 1, output = "matrix") 
+{
+  input <- inputw(wave = wave, f = f, channel = channel)
+  wave <- input$w
+  f <- input$f
+  bit <- input$bit
+  rm(input)$w
+  n <- nrow(wave)
+  if (g == f) 
+    wave1 <- wave
+  if (g < f) {
+    r <- f/g
+    wave1 <- wave[seq(1, n, by = r), 1]
+  }
+  if (g > f) {
+    s <- (n * g)/f
+    wave1 <- approx(wave, n = s)$y
+  }
+  wave1 <- outputw(wave = wave1, f = g, format = output)
+  return(wave1)
+}
+
+rnd_mix <- function(input.dir1, input.dir2, input.dir3,
+                    output.dir1, output.dir2, output.dir3, n.sec=5,
+                    nrand.in = 25, nrand.out = 3, seed=123, samp.rate=44100){
+  set.seed(seed)
+  wav_list1 <- list.files(input.dir1, full.names = T, pattern = ".wav")
+  wav_list2 <- list.files(input.dir2, full.names = T, pattern = ".wav")
+  wav_list3 <- list.files(input.dir3, full.names = T, pattern = ".wav")
+  rnd_list1 <- wav_list1[sample(length(wav_list1), nrand.in)]
+  rnd_list2 <- wav_list2[sample(length(wav_list2), nrand.in)]
+  rnd_list3 <- wav_list3[sample(length(wav_list3), nrand.in)]
+  
+  # Randomly get single samples at samp.rate, and cut out a n.sec chunk
+  subsamp1 <- lapply(1:length(rnd_list1), function(i) readWave(rnd_list1[[i]]))
+  subsamp1 <- lapply(1:length(rnd_list1), function(i) my_resamp(subsamp1[[i]],
+                                                              g = samp.rate,
+                                                              output = "Wave"))
+  subsamp1_dur <- unlist(lapply(1:length(rnd_list1), function(i) duration(subsamp1[[i]])))
+  subsamp_rnd_start <- runif(5, min=0, max=unlist(subsamp1_dur)-n.sec)
+  for(j in 1:length(rnd_list1)){
+    if(subsamp1_dur[[j]] <= n.sec)
+      stop("Duration length to long for cutting")
+    
+  }
+  subsamp1 <- lapply(1:length(rnd_list1), function(i) cutw(subsamp1[[i]],
+                                                           from=subsamp_rnd_start[i],
+                                                           to=subsamp_rnd_start[i]+n.sec,
+                                                           output="Wave"))
+  
+}
