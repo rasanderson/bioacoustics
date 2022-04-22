e18bfd78b429721ecce634ac63c7fec3ae75f147
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2022-04-22 21:36:56 +0100

    Successfully ran DWT on ECG data. Test score lower than in blog.

--- wavelet.py
+++ wavelet.py
@@ -4,7 +4,115 @@
 import pywt
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import os
 
+from sklearn.ensemble import GradientBoostingClassifier
+
+# From previous blog (seems to need function)
+from scipy.fftpack import fft
+
+def get_fft_values(y_values, T, N, f_s):
+    f_values = np.linspace(0.0, 1.0/(2.0*T), N//2)
+    fft_values_ = fft(y_values)
+    fft_values = 2.0/N * np.abs(fft_values_[0:N//2])
+    return f_values, fft_values
+
+
+# Section 2.1 From Fourier transform to Wavelet transform
+t_n = 1
+N = 100000
+T = t_n / N
+f_s = 1/T
+
+xa = np.linspace(0, t_n, num=N)
+xb = np.linspace(0, t_n/4, num=int(N/4))
+
+frequencies = [4, 30, 60, 90]
+y1a, y1b = np.sin(2*np.pi*frequencies[0]*xa), np.sin(2*np.pi*frequencies[0]*xb)
+y2a, y2b = np.sin(2*np.pi*frequencies[1]*xa), np.sin(2*np.pi*frequencies[1]*xb)
+y3a, y3b = np.sin(2*np.pi*frequencies[2]*xa), np.sin(2*np.pi*frequencies[2]*xb)
+y4a, y4b = np.sin(2*np.pi*frequencies[3]*xa), np.sin(2*np.pi*frequencies[3]*xb)
+
+composite_signal1 = y1a + y2a + y3a + y4a
+composite_signal2 = np.concatenate([y1b, y2b, y3b, y4b])
+
+f_values1, fft_values1 = get_fft_values(composite_signal1, T, N, f_s)
+f_values2, fft_values2 = get_fft_values(composite_signal2, T, N, f_s)
+
+fig, axarr = plt.subplots(nrows=2, ncols=2, figsize=(12,8))
+axarr[0,0].plot(xa, composite_signal1)
+axarr[1,0].plot(xa, composite_signal2)
+axarr[0,1].plot(f_values1, fft_values1)
+axarr[1,1].plot(f_values2, fft_values2)
+(...)
+plt.tight_layout()
+plt.show()
+
+# Section 2.3
+import pywt
+print(pywt.families(short=False))
+
+discrete_wavelets = ['db5', 'sym5', 'coif5', 'bior2.4']
+continuous_wavelets = ['mexh', 'morl', 'cgau5', 'gaus5']
+
+list_list_wavelets = [discrete_wavelets, continuous_wavelets]
+list_funcs = [pywt.Wavelet, pywt.ContinuousWavelet]
+
+fig, axarr = plt.subplots(nrows=2, ncols=4, figsize=(16,8))
+for ii, list_wavelets in enumerate(list_list_wavelets):
+    func = list_funcs[ii]
+    row_no = ii
+    for col_no, waveletname in enumerate(list_wavelets):
+        wavelet = func(waveletname)
+        family_name = wavelet.family_name
+        biorthogonal = wavelet.biorthogonal
+        orthogonal = wavelet.orthogonal
+        symmetry = wavelet.symmetry
+        if ii == 0:
+            _ = wavelet.wavefun()
+            wavelet_function = _[0]
+            x_values = _[-1]
+        else:
+            wavelet_function, x_values = wavelet.wavefun()
+        if col_no == 0 and ii == 0:
+            axarr[row_no, col_no].set_ylabel("Discrete Wavelets", fontsize=16)
+        if col_no == 0 and ii == 1:
+            axarr[row_no, col_no].set_ylabel("Continuous Wavelets", fontsize=16)
+        axarr[row_no, col_no].set_title("{}".format(family_name), fontsize=16)
+        axarr[row_no, col_no].plot(x_values, wavelet_function)
+        axarr[row_no, col_no].set_yticks([])
+        axarr[row_no, col_no].set_yticklabels([])
+
+plt.tight_layout()
+plt.show()
+
+import pywt
+import matplotlib.pyplot as plt
+
+db_wavelets = pywt.wavelist('db')[:5]
+print(db_wavelets)
+
+fig, axarr = plt.subplots(ncols=5, nrows=5, figsize=(20,16))
+fig.suptitle('Daubechies family of wavelets', fontsize=16)
+for col_no, waveletname in enumerate(db_wavelets):
+    wavelet = pywt.Wavelet(waveletname)
+    no_moments = wavelet.vanishing_moments_psi
+    family_name = wavelet.family_name
+    for row_no, level in enumerate(range(1,6)):
+        wavelet_function, scaling_function, x_values = wavelet.wavefun(level = level)
+        axarr[row_no, col_no].set_title("{} - level {}\n{} vanishing moments\n{} samples".format(
+            waveletname, level, no_moments, len(x_values)), loc='left')
+        axarr[row_no, col_no].plot(x_values, wavelet_function, 'bD--')
+        axarr[row_no, col_no].set_yticks([])
+        axarr[row_no, col_no].set_yticklabels([])
+plt.tight_layout()
+plt.subplots_adjust(top=0.9)
+plt.show()
+
+# Section 2.4 has no code
+
+# Section 2.5
 x = np.linspace(0, 1, num=2048)
 chirp_signal = np.sin(250 * np.pi * x**2)
     
@@ -30,11 +138,42 @@ for ii in range(5):
 plt.tight_layout()
 plt.show()
 
+# Section 3.2.1
+def read_signals_ucihar(filename):
+    with open(filename, 'r') as fp:
+        data = fp.read().splitlines()
+        data = map(lambda x: x.rstrip().lstrip().split(), data)
+        data = [list(map(float, line)) for line in data]
+    return data
 
+def read_labels_ucihar(filename):        
+    with open(filename, 'r') as fp:
+        activities = fp.read().splitlines()
+        activities = list(map(int, activities))
+    return activities
 
+def load_ucihar_data(folder):
+    train_folder = folder + 'train/InertialSignals/'
+    test_folder = folder + 'test/InertialSignals/'
+    labelfile_train = folder + 'train/y_train.txt'
+    labelfile_test = folder + 'test/y_test.txt'
+    train_signals, test_signals = [], []
+    for input_file in os.listdir(train_folder):
+        signal = read_signals_ucihar(train_folder + input_file)
+        train_signals.append(signal)
+    train_signals = np.transpose(np.array(train_signals), (1, 2, 0))
+    for input_file in os.listdir(test_folder):
+        signal = read_signals_ucihar(test_folder + input_file)
+        test_signals.append(signal)
+    test_signals = np.transpose(np.array(test_signals), (1, 2, 0))
+    train_labels = read_labels_ucihar(labelfile_train)
+    test_labels = read_labels_ucihar(labelfile_test)
+    return train_signals, train_labels, test_signals, test_labels
 
+folder_ucihar = './datasets/UCI_HAR/' 
+train_signals_ucihar, train_labels_ucihar, test_signals_ucihar, test_labels_ucihar = load_ucihar_data(folder_ucihar)
 
-
+# Section 3.3
 (cA1, cD1) = pywt.dwt(signal, 'db2', 'smooth')
 reconstructed_signal = pywt.idwt(cA1, cD1, 'db2', 'smooth')
 
@@ -43,3 +182,155 @@ ax.plot(signal, label='signal')
 ax.plot(reconstructed_signal, label='reconstructed signal', linestyle='--')
 ax.legend(loc='upper left')
 plt.show()
+
+coeffs = pywt.wavedec(signal, 'db2', level=8)
+reconstructed_signal = pywt.waverec(coeffs, 'db2')
+
+fig, ax = plt.subplots(figsize=(8,4))
+ax.plot(signal[:1000], label='signal')
+ax.plot(reconstructed_signal[:1000], label='reconstructed signal', linestyle='--')
+ax.legend(loc='upper left')
+ax.set_title('de- and reconstruction using wavedec()')
+plt.show()
+
+
+
+# Section 3.4
+DATA_FOLDER = './datasets/FEMTO_bearing/Training_set/Bearing1_1/'
+filename = 'acc_01210.csv'
+df = pd.read_csv(DATA_FOLDER + filename, header=None)
+signal = df[4].values
+
+def lowpassfilter(signal, thresh = 0.63, wavelet="db4"):
+    thresh = thresh*np.nanmax(signal)
+    coeff = pywt.wavedec(signal, wavelet, mode="per" )
+    coeff[1:] = (pywt.threshold(i, value=thresh, mode="soft" ) for i in coeff[1:])
+    reconstructed_signal = pywt.waverec(coeff, wavelet, mode="per" )
+    return reconstructed_signal
+
+fig, ax = plt.subplots(figsize=(12,8))
+ax.plot(signal, color="b", alpha=0.5, label='original signal')
+rec = lowpassfilter(signal, 0.4)
+ax.plot(rec, 'k', label='DWT smoothing}', linewidth=2)
+ax.legend()
+ax.set_title('Removing High Frequency Noise with DWT', fontsize=18)
+ax.set_ylabel('Signal Amplitude', fontsize=16)
+ax.set_xlabel('Sample No', fontsize=16)
+plt.show()
+
+
+# Section 3.5
+# Section 3.5.2
+def calculate_entropy(list_values):
+    counter_values = Counter(list_values).most_common()
+    probabilities = [elem[1]/len(list_values) for elem in counter_values]
+    entropy=scipy.stats.entropy(probabilities)
+    return entropy
+
+
+def calculate_statistics(list_values):
+    n5 = np.nanpercentile(list_values, 5)
+    n25 = np.nanpercentile(list_values, 25)
+    n75 = np.nanpercentile(list_values, 75)
+    n95 = np.nanpercentile(list_values, 95)
+    median = np.nanpercentile(list_values, 50)
+    mean = np.nanmean(list_values)
+    std = np.nanstd(list_values)
+    var = np.nanvar(list_values)
+    rms = np.nanmean(np.sqrt(list_values**2))
+    return [n5, n25, n75, n95, median, mean, std, var, rms]
+
+# Next function fails initially. &gt; instead of > due to WordPress error?
+def calculate_crossings(list_values):
+    zero_crossing_indices = np.nonzero(np.diff(np.array(list_values) > 0))[0]
+    no_zero_crossings = len(zero_crossing_indices)
+    mean_crossing_indices = np.nonzero(np.diff(np.array(list_values) > np.nanmean(list_values)))[0]
+    no_mean_crossings = len(mean_crossing_indices)
+    return [no_zero_crossings, no_mean_crossings]
+
+
+def get_features(list_values):
+    entropy = calculate_entropy(list_values)
+    crossings = calculate_crossings(list_values)
+    statistics = calculate_statistics(list_values)
+    return [entropy] + crossings + statistics
+
+
+
+
+def load_ecg_data(filename):
+    raw_data = sio.loadmat(filename)
+    list_signals = raw_data['ECGData'][0][0][0]
+    list_labels = list(map(lambda x: x[0][0], raw_data['ECGData'][0][0][1]))
+    return list_signals, list_labels
+
+
+##########
+import scipy.io as sio
+filename = './datasets/ECG_data/ECGData.mat'
+data_ecg, labels_ecg = load_ecg_data(filename)
+training_size = int(0.6*len(labels_ecg))
+train_data_ecg = data_ecg[:training_size]
+test_data_ecg = data_ecg[training_size:]
+train_labels_ecg = labels_ecg[:training_size]
+test_labels_ecg = labels_ecg[training_size:]
+
+def get_uci_har_features(dataset, labels, waveletname):
+    uci_har_features = []
+    for signal_no in range(0, len(dataset)):
+        features = []
+        for signal_comp in range(0,dataset.shape[2]):
+            signal = dataset[signal_no, :, signal_comp]
+            list_coeff = pywt.wavedec(signal, waveletname)
+            for coeff in list_coeff:
+                features += get_features(coeff)
+        uci_har_features.append(features)
+    X = np.array(uci_har_features)
+    Y = np.array(labels)
+    return X, Y
+
+def get_ecg_features(ecg_data, ecg_labels, waveletname):
+    list_features = []
+    list_unique_labels = list(set(ecg_labels))
+    list_labels = [list_unique_labels.index(elem) for elem in ecg_labels]
+    for signal in ecg_data:
+        list_coeff = pywt.wavedec(signal, waveletname)
+        features = []
+        for coeff in list_coeff:
+            features += get_features(coeff)
+        list_features.append(features)
+    return list_features, list_labels
+
+from collections import Counter
+import scipy
+from scipy import stats
+
+X_train_ecg, Y_train_ecg = get_ecg_features(train_data_ecg, train_labels_ecg, 'db4')
+X_test_ecg, Y_test_ecg = get_ecg_features(test_data_ecg, test_labels_ecg, 'db4')
+
+X_train_ucihar, Y_train_ucihar = get_uci_har_features(train_signals_ucihar, train_labels_ucihar, 'rbio3.1')
+X_test_ucihar, Y_test_ucihar = get_uci_har_features(test_signals_ucihar, test_labels_ucihar, 'rbio3.1')
+
+
+cls = GradientBoostingClassifier(n_estimators=2000)
+cls.fit(X_train_ecg, Y_train_ecg)
+train_score = cls.score(X_train_ecg, Y_train_ecg)
+test_score = cls.score(X_test_ecg, Y_test_ecg)
+print("Train Score for the ECG dataset is about: {}".format(train_score))
+test_score3 = np.around(test_score, 3)
+print("Test Score for the ECG dataset is about: {}".format(test_score3))
+
+### 
+
+cls = GradientBoostingClassifier(n_estimators=2000)
+cls.fit(X_train_ucihar, Y_train_ucihar)
+train_score = cls.score(X_train_ucihar, Y_train_ucihar)
+test_score = cls.score(X_test_ucihar, Y_test_ucihar)
+print("Train Score for the UCI-HAR dataset is about: {}".format(train_score))
+test_score3 = np.around(test_score, 3)
+print("Test Score for the UCI-HAR dataset is about: {}".format(test_score3))
+
+*** Train Score for the ECG dataset is about: 1.0
+*** Test Score for the ECG dataset is about: 0.93
+*** Train Score for the UCI_HAR dataset is about: 1.0
+*** Test Score for the UCI-HAR dataset is about: 0.95
